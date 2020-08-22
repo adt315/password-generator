de513f5b4244bb8621f2eb36560c01877a4f1870
adt315 <angela_dyan@hotmail.com>
2020-08-22 12:01:57 -0400

    changes in vars, etc

--- script.js
+++ script.js
@@ -6,19 +6,25 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChar = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '//', ']', '^', ';', '`', '{', '|', '}', '~'];
 
+var passwordLength = 8;
 
 // function getUserInput() {
 function getLength() {
   var initialLength = prompt("How many characters would you like your password to be?           (Password must be between 8 and 128 characters)");
   
     if (initialLength > 8 && initialLength < 128) {
-      var passwordLength = initialLength;
-      console.log(passwordLength);
+      passwordLength = initialLength;
+      
     } else {
     alert("Please enter a number between 8 and 128");
     getLength();
     } 
 } 
+
+var hasLowercase = false;
+var hasUppercase = false;
+var hasNumbers = false;
+var hasSpecialChar = false;
 
 function getCharacters() {
   var initialLowercase = confirm("Do you want your password to include lowercase letters?");
@@ -27,33 +33,37 @@ function getCharacters() {
   var initialSpecialChar = confirm("Do you want your password to include special characters?");
 
     if (initialLowercase === true || initialUppercase === true || initialNumbers === true || initialSpecialChar === true) {
-      var hasLowercase = initialLowercase;
-      console.log(hasLowercase);
-      var hasUppercase = initialUppercase;
-      console.log(hasUppercase);
-      var hasNumbers = initialNumbers;
-      console.log(hasNumbers);
-      var hasSpecialChar = initialSpecialChar;
-      console.log(hasSpecialChar);
+      hasLowercase = initialLowercase;
+      hasUppercase = initialUppercase;
+      hasNumbers = initialNumbers;
+      hasSpecialChar = initialSpecialChar;
     } else {
       alert("Please select at least one type of character");
       getCharacters();
     } 
 }
 
-function getPasswordOptions() {
-  var passwordOptions = {
-    passwordLength: passwordLength,
-    hasLowercase: hasLowercase,
-    hasUppercase: hasUppercase,
-    hasNumbers: hasNumbers,
-    hasSpecialChar: hasSpecialChar
-  }
-  return passwordOptions
-}
+// function getPasswordOptions() {
+//   var passwordOptions = {
+//     passwordLength: passwordLength,
+//     hasLowercase: hasLowercase,
+//     hasUppercase: hasUppercase,
+//     hasNumbers: hasNumbers,
+//     hasSpecialChar: hasSpecialChar
+//   }
+//   return passwordOptions
+// }
   // (after they answer each prompt make sure at least one character type is selected)(if loercaseLetters and uppercaseLetters and hasNubers and specialChar all = false, alert please select at least one type of character)
   
-// function generatePassword () {
+function generatePassword () {
+  getLength();
+  console.log(passwordLength);
+  getCharacters();
+  console.log(hasLowercase);
+  console.log(hasUppercase);
+  console.log(hasNumbers);
+  console.log(hasSpecialChar);
+}
 //   var options = getPasswordOptions();
 //   console.log(options);
 
@@ -78,4 +88,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", getCharacters);
+generateBtn.addEventListener("click", generatePassword);
